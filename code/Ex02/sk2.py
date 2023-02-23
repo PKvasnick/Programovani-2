e3c1eabca28b0c9408dd5e955302877fbc63f179
Peter Kvasnicka <kvasnicka@ipnp.troja.mff.cuni.cz>
2023-02-23 17:25:22 +0100

    Cviceni2_uprava2

--- code/Ex02/sk2.py
+++ code/Ex02/sk2.py
@@ -1,19 +1,26 @@
-import sys
+class CaseInsensitiveSet:
 
-class CaseInsensitiveSet(set):
-    def add(self, value):
-        set.add(self, value.casefold())
+    def __init__(self):
+        self.set_ = set()
 
-    def __contains__(self, value):
-        return set.__contains__(self, value.casefold())
+    def add(self, item):
+        self.set.add(item.casefold())
 
-    def discard(self, value):
-        set.discard(self, value.casefold())
+    def __contains__(self, item):
+        return item.casefold() in self.set
 
-    def remove(self, value):
-        set.remove(self, value.casefold())
+    def remove(self, item):
+        self.set.remove(item.casefold())
 
+    def discard(self, item):
+        self.set.discard(item.casefold())
 
+    def __str__(self):
+        return str(self.set)
+
+    
+
+print("Vytvoř:")
 s = CaseInsensitiveSet()
 
 s.add("Rodrigo")
@@ -22,14 +29,17 @@ s.add("RODRIGO")
 
 print(s)  # CaseInsensitiveSet({'rodrigo', 'mathspp'})
 
-print("In: ", "RODRIGO" in s)  # True
+print("Contains: ")
+print("RODRIGO" in s)  # True
 
+print("Discard")
 s.discard("MaThSpP")  # Try to remove "mathspp"
-print("Discard: ", s)  # CaseInsensitiveSet({'rodrigo'})
+print(s)  # CaseInsensitiveSet({'rodrigo'})
 
 s.discard("mathspp")  # Try to remove "mathspp"
 print(s)  # CaseInsensitiveSet({'rodrigo'})
 
+print("AddRemove")
 s.add("mathspp")
 s.remove("rodrigo")  # Remove "rodrigo" and error if not present
 print(s)  # CaseInsensitiveSet({'mathspp'})
