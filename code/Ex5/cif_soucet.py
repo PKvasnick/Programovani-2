00a15bd7f8cedaf782b15c57d6a0ae7f58e56fd0
Peter Kvasnicka <kvasnicka@ipnp.troja.mff.cuni.cz>
2023-03-17 16:31:02 +0100

    Ex5_2

--- code/Ex5/cif_soucet.py
+++ code/Ex5/cif_soucet.py
@@ -1,5 +1,3 @@
-from timeit import Timer
-
 def cif_soucet_1(cislo: int) -> int:
     """mod 10 dává číslici, div 10 zbytek"""
     soucet = 0
@@ -18,12 +16,3 @@ def cif_soucet_3(cislo: int) -> int:
         cislo, zbytek = divmod(cislo, 10)
         soucet += zbytek
     return soucet
-
-t1 = Timer("cif_soucet_1(123456789)", "from speedy import cif_soucet_1")
-print(1, t1.timeit(number = 1000000))
-
-t2 = Timer("cif_soucet_2(123456789)", "from speedy import cif_soucet_2")
-print(2, t2.timeit(number = 1000000))
-
-t3 = Timer("cif_soucet_3(123456789)", "from speedy import cif_soucet_3")
-print(3, t3.timeit(number = 1000000))
