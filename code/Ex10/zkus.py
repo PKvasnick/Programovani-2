5be9ec442a77c710b5f5182340f6dda938f79f53
Peter Kvasnicka <kvasnicka@ipnp.troja.mff.cuni.cz>
2023-04-23 16:37:07 +0200

    Ex09 pokus o opravu obrazkov

--- code/Ex10/zkus.py
+++ code/Ex10/zkus.py
@@ -1,25 +1,25 @@
-class Expression:
+class Node:
     ...
 
 
-class Constant:
+class Constant(Node):
     def __init__(self, value):
         self.value = value
 
     def __str__(self):
-        return str(self.value)
+        return f"{self.value}"
 
-    def eval(self, env):
+    def eval(self):
         return self.value
 
     def derivative(self, by):
         return Constant(0)
 
-    def purge(self):
+    def prune(self):
         return self
 
 
-class Variable(Expression):
+class Variable(Node):
     def __init__(self, name):
         self.name = name
 
@@ -27,25 +27,32 @@ class Variable(Expression):
         return self.name
 
     def eval(self, env):
-        return env[self.name]
+        return f"{env[self.name]}"
 
     def derivative(self, by):
-        if by == self.name:
+        if self.name == by:
             return Constant(1)
         else:
             return Constant(0)
 
-        def purge(self):
-            return self
+    def prune(self):
+        return self
 
 
-class Plus(Expression):
+def isZeroConstant(c):
+    return isinstance(c, Constant) and c.value == 0
+
+def isUnitConstant(c):
+    return isinstance(c, Constant) and c.value == 1
+
+
+class Plus(Node):
     def __init__(self, left = None, right = None):
         self.left = left
         self.right = right
 
     def __str__(self):
-        return f"({self.left} + {self.right})"
+        return f"{self.left} + {self.right}"
 
     def eval(self, env):
         return self.left.eval(env) + self.right.eval(env)
@@ -56,16 +63,26 @@ class Plus(Expression):
             self.right.derivative(by)
         )
 
-    def purge(self):
+    def prune(self):
+        self.left = self.left.prune()
+        self.right = self.right.prune()
+        if isZeroConstant(self.left):
+            if isZeroConstant(self.right):
+                return Constant(0)
+            else:
+                return self.right
+        if isZeroConstant(self.right):
+            return self.left
+        return self
 
 
-class Times(Expression):
+class Times(Node):
     def __init__(self, left=None, right=None):
         self.left = left
         self.right = right
 
     def __str__(self):
-        return f"({self.left} * {self.right})"
+        return f"{self.left} * {self.right}"
 
     def eval(self, env):
         return self.left.eval(env) * self.right.eval(env)
@@ -73,30 +90,46 @@ class Times(Expression):
     def derivative(self, by):
         return Plus(
             Times(
-                self.left,
-                self.right.derivative(by)
-            ),
-            Times(
                 self.left.derivative(by),
                 self.right
+            ),
+            Times(
+                self.left,
+                self.right.derivative(by)
             )
         )
 
+    def prune(self):
+        self.left = self.left.prune()
+        self.right = self.right.prune()
+        if isZeroConstant(self.left) | isZeroConstant(self.right):
+            return Constant(0)
+        if isUnitConstant(self.left):
+            if isUnitConstant(self.right):
+                return Constant(1)
+            else:
+                return self.right
+        if isUnitConstant(self.right):
+            return self.left
+        return self
 
-def main():
-    vyraz = Plus(
+
+def main() -> None:
+    tree = Plus(
         Variable("x"),
         Times(
             Constant(5),
             Variable("y")
         )
     )
-    print(vyraz)
-    print(vyraz.derivative(by = "x"))
-    print(vyraz.derivative(by = "y"))
+
+    print(tree)
+    # print(tree.eval(env={"x":2, "y":1}))
+    print(tree.derivative(by = "x"))
+    print(tree.derivative(by = "x").prune())
+    print(tree.derivative(by = "y"))
+    print(tree.derivative(by = "y").prune())
+
 
 if __name__ == "__main__":
     main()
-
-
-
