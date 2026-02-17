9b13b0d3d20ec44e2783ee79f33c6535b2ecbcf8
Peter Kvasnicka <kvasnicka@ipnp.troja.mff.cuni.cz>
2026-02-17 09:45:15 +0100

    Ex1_final

--- code/Ex01/soucet_reduce.py
+++ code/Ex01/soucet_reduce.py
@@ -14,15 +14,13 @@ def add_and_carry(accumulator: tuple[int, int], digits: tuple[int, int]) -> tupl
     return divmod(new_sum, 10)  # returns (carry, digit_sum)
 
 
-def sum_by_digits(a0: list[int], b0: list[int]) -> list[int]:
+def sum_by_digits(a: list[int], b: list[int]) -> list[int]:
     """
     Sums two integers given as lists of digits.
     :param a0: list of digits of the first number
     :param b0: list of digits of the second number
     :return: list of digits of the sum
     """
-    a = a0.copy()     # to protect from side effects
-    b = b0.copy()
     tuples = list(accumulate(zip_longest(a, b, fillvalue=0), func=add_and_carry, initial=(0,0)))
     tuples.reverse()
     tuples.pop()    # remove initial state
@@ -36,16 +34,16 @@ def main() -> None:
     """
     Demonstrates the sum_by_digits function.
     """
-    a0 = [int(c) for c in input()]
-    b0 = [int(c) for c in input()]
-    print("First number: ", *a0, sep = "")
-    print("Second number: ",*b0, sep = "")
-    digit_sum = sum_by_digits(a0, b0)
-    print(*digit_sum, sep="")
-    print("Kontrola:")
-    a0_int = int("".join([str(d) for d in a0]))
-    b0_int = int("".join([str(d) for d in b0]))
-    print(a0_int + b0_int)
+    a = [int(c) for c in input()]
+    b = [int(c) for c in input()]
+    print("First number: ", *a, sep = "")
+    print("Second number: ",*b, sep = "")
+    digit_sum = sum_by_digits(a, b)
+    print("Sum: ", *digit_sum, sep="")
+    print("Check:")
+    a_int = int("".join([str(d) for d in a]))
+    b_int = int("".join([str(d) for d in b]))
+    print("Int addition: ", a_int + b_int)
 
 
 if __name__ == "__main__":
