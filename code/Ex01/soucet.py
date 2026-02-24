9b48f34e1b6ef05a612114a48f0dd953a0ce2c13
Peter Kvasnicka <kvasnicka@ipnp.troja.mff.cuni.cz>
2026-02-24 09:49:46 +0100

    Ex02

--- code/Ex01/soucet.py
+++ code/Ex01/soucet.py
@@ -8,15 +8,17 @@ add_table = {(i, j) : divmod(i + j, 10) for i in range(10) for j in range(10)}
 a = [int(x) for x in input()]
 b = [int(x) for x in input()]
 
-result = []
+reversed_result = []	# Dobré jméno (C) Dan Ransdorf
 carry = 0
 for da, db in zip_longest(reversed(a), reversed(b), fillvalue=0):
-    carry, digit_sum = add_table[(da, db)]
-    result.append(digit_sum)
+    carry_1, da = add_table[(da, carry)]
+    carry_2, digit_sum = add_table[(da, db)]
+    _, carry = add_table[(carry_1, carry_2)]
+    reversed_result.append(digit_sum)
 if carry > 0:
-    result.append(carry)
+    reversed_result.append(carry)
 
 a_print = "".join(map(str, a))
 b_print = "".join(map(str, b))
-result_print = "".join(map(str, reversed(result)))
+result_print = "".join(map(str, reversed(reversed_result)))
 print(f"{a_print} + {b_print} = {result_print}")
